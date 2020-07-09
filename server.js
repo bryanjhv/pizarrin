964614e24b314e3e8d4e4e7bc4f4925ea3ec5340
Bryan Horna <bryanjhv@gmail.com>
2020-07-08 20:10:12 -0500

    Implementar base del servidor

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const SocketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = SocketIO(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  // TODO: Manejar conexión del cliente
+});
+
+const { PORT = 3000 } = process.env;
+server.listen(PORT, () => {
+  console.log(`Servidor listo en puerto ${PORT}`);
+
+  function close() {
+    server.close((err) => {
+      if (err) console.error(err);
+      process.exit(err ? 1 : 0);
+    });
+  }
+  process.once("SIGINT", close).once("SIGTERM", close);
+});
